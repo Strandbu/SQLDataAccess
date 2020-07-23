fa3d8e0814e3845d1b88be162adadfd4597ac801
Strandbu <sandbustrand@hotmail.com>
2020-07-23 12:28:18 +0200

    First successfull build

--- FormUI/DataAccess.cs
+++ FormUI/DataAccess.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace FormUI
 {
@@ -10,7 +13,12 @@ namespace FormUI
     {
         public List<Person> GetPeople(string lastname)
         {
-            throw new NotImplementedException();
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CNNVal("SampleDB")))
+            {
+                var output =  connection.Query<Person>($"select * from People where LastName = '{ lastname }'").ToList();
+                //var output = connection.Query<Person>("dbo.People_GetByLastName @LastName").ToList();
+                return   output;
+            }
         }
     }
 }
